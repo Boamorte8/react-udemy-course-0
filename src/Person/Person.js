1037f77e8c23bcfc5961a827dc4980237b76a472
Boamorte8 <salazar.z.esteban@gmail.com>
2019-01-02 15:54:52 -0500

    First commit

---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Person.css';
+
+const person = () => {
+    // return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
+}
+
+export default person;
