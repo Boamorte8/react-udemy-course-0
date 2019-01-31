296d8c1e7f6f66d961308f6bdf2103e48defce2e
Esteban Salazar Zapata <esteban.salazar@estebansalazarMB.local>
2019-01-31 10:10:36 -0500

    Add Radium library

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -35,39 +36,46 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '.5rem',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App </h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={() => this.switchNameHandler('Sebastian')}>
-            Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Santiago')}
-          changed={this.nameChangedHandler}>
-            My Hobbies: Walking
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}/>
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App </h1>
+          <p>This is really working!</p>
+          <button
+            style={style}
+            onClick={() => this.switchNameHandler('Sebastian')}>
+              Switch Name
+          </button>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Santiago')}
+            changed={this.nameChangedHandler}>
+              My Hobbies: Walking
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}/>
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!!'));
   }
 }
 
-export default App;
+export default Radium(App);
